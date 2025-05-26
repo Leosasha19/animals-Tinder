ed9e0b58626e379958d91e212643a74473e0d7b2
а <a@MacBook-Pro-a.local>
2025-05-26 14:30:56 +0300

    Restructured the project, introduced separate client and backend folders, and updated the README

---
+++ client/src/pages/Liked/Liked.tsx
@@ -0,0 +1,62 @@
+import React, {useCallback, useEffect, useState} from 'react';
+import {useAppDispatch, useAppSelector} from "../../hooks/redux.ts";
+import {addAnimal, addRandomAnimal, getAnimals, selectAnimalsState} from "../../features/AnimalDataSlice.ts";
+import './Liked.scss';
+
+const Liked = () => {
+
+    const animalsState = useAppSelector(selectAnimalsState);
+    const dispatch = useAppDispatch();
+    const [oneRandomAnimal, setOneRandomAnimal] = useState(null);
+
+    useEffect(() => {
+        dispatch(getAnimals())
+    }, []);
+
+
+    useEffect(() => {
+        if (animalsState?.animals) {
+            const likedAnimalsData = animalsState.animals.filter((animal) => animal.like === true);
+            if (likedAnimalsData.length > 0) {
+                const randomAn = likedAnimalsData[Math.floor(Math.random() * likedAnimalsData.length)];
+                setOneRandomAnimal(randomAn);
+            }
+        }
+    }, [animalsState.animals]);
+
+    useEffect(() => {
+        if (oneRandomAnimal) {
+            const { id, urlimg, commentary, iscat, like } = oneRandomAnimal;
+            dispatch(addRandomAnimal({
+                id: id,
+                imageURL: urlimg,
+                comment: commentary,
+                isACat: iscat,
+                like: like,
+            }));
+        }
+    }, [oneRandomAnimal, dispatch]);
+
+
+    const addCurrentAnimal = useCallback(() => {
+        if (animalsState?.animals) {
+            const likedAnimalsData = animalsState.animals.filter((animal) => animal.like === true);
+            if (likedAnimalsData.length > 0) {
+                const randomAn = likedAnimalsData[Math.floor(Math.random() * likedAnimalsData.length)];
+                setOneRandomAnimal(randomAn);
+            }
+        }
+    },[animalsState])
+
+    return (
+        <div className={"mainContainer"}>
+            <div className={"mainContainer__animalsBox"}>
+                <img src={animalsState.currentAnimal.imageURL || null} alt=""/>
+                <div>{animalsState.currentAnimal.comment}</div>
+                <button onClick={addCurrentAnimal}>Next</button>
+            </div>
+        </div>
+    );
+};
+
+export default Liked;
---
+++ client/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,47 @@
+import AnimalCard from "../../components/AnimalCard/AnimalCard.tsx";
+import React, {useState} from "react";
+import {getCatApi, getDogApi, selectAnimalsState} from "../../features/AnimalDataSlice.ts";
+import {useNavigate} from "react-router-dom";
+import './MainPage.scss'
+import {useAppSelector} from "../../hooks/redux.ts";
+
+const MainPage = () => {
+    const navigate = useNavigate();
+    const animalState = useAppSelector(selectAnimalsState)
+    const [isCat, setIsCat] = useState(true);
+
+    const goLikedPage = () => {
+        navigate('/liked')
+    }
+
+    return (
+        <div className={"mainContainer"}>
+            {animalState.error ? (
+                <div className="error-message">{"Упс... Похоже что то сломалось"}</div>
+            ) : (
+                <>
+                    <div className={"mainContainer__topLine"}>
+                        <button
+                            onClick={() => setIsCat(true)}
+                            className={isCat ? "mainContainer__topLine__switchButtonsActive" : "mainContainer__topLine__switchButtons"}>CATS
+                        </button>
+                        <button
+                            onClick={() => setIsCat(false)}
+                            className={isCat ? "mainContainer__topLine__switchButtons" : "mainContainer__topLine__switchButtonsActive"}>DOGS
+                        </button>
+                        <button
+                            onClick={goLikedPage}
+                            className={"mainContainer__topLine__liked"}>❤️
+                        </button>
+                    </div>
+                    <div className={"mainContainer__animalCard"}>
+                        <AnimalCard fetchAnimal={isCat ? getCatApi : getDogApi}/>
+                    </div>
+                </>
+            )}
+
+        </div>
+    );
+};
+
+export default MainPage;
