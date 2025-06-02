dd4c4a44de832b2873a873f005901f8105438e2a
Leosasha19 <Leosasha19@yandex.ru>
2025-06-02 13:03:12 +0300

    Configure ESLint and Prettie

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,17 +1,17 @@
-import MainPage from "../pages/MainPage/MainPage.tsx";
-import {Route, Routes} from "react-router-dom";
-import Liked from "../pages/Liked/Liked.tsx";
-import './App.scss'
+import MainPage from '../pages/MainPage/MainPage.tsx';
+import { Route, Routes } from 'react-router-dom';
+import Liked from '../pages/Liked/Liked.tsx';
+import './App.scss';
 
 function App() {
   return (
     <>
-        <Routes>
-            <Route index element={<MainPage />}/>
-            <Route path={'/liked'} element={<Liked/>}/>
-        </Routes>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path={'/liked'} element={<Liked />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
